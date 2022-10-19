fafed3a932500b02a2b6ad192783c42e025ea93d
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-10-19 22:28:12 +0530

    done

---
+++ db.js
@@ -0,0 +1,17 @@
+const items = require("./data/team");
+const info = require("./data/info");
+const player = require("./data/player");
+const login = require("./data/login");
+
+
+
+module.exports = {
+  items,
+  info,
+  player,
+  login
+ 
+}
+
+
+
